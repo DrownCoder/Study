122fecf8d7ddd54172fc82215f8a1921837530e6
DZX <2994734692@qq.com>
2017-02-27 16:29:33 +0800

    Broadcast中的有序广播和本地广播

--- app/src/main/java/com/xuan/study/study/activity/MainActivity.java
+++ app/src/main/java/com/xuan/study/study/activity/MainActivity.java
@@ -1,31 +1,108 @@
 package com.xuan.study.study.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.PersistableBundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.xuan.study.study.R;
 import com.xuan.study.study.service.WeatherService;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String BROADCAST_ACTION = "XUAN";
+    /**
+     * 广播接收器
+     */
+
+    public BroadcastReceiver oneBroadcastReciver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(BROADCAST_ACTION))
+                Toast.makeText(context,"This is One! "+getResultData(),Toast.LENGTH_SHORT).show();
+            //丢弃广播
+            //abortBroadcast();
+            //setResultData("One is　your first");
+        }
+    };
+    public BroadcastReceiver twoBroadcastReciver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals(BROADCAST_ACTION))
+                Toast.makeText(context,"This is Two! "+getResultData(),Toast.LENGTH_SHORT).show();
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * 弹一个DialogAcitivity
+         */
         Button dialog = (Button) findViewById(R.id.bt_id_dialog);
-        dialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                //Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:13888888888"));
-                startActivity(intent);
-            }
-        });
+        dialog.setOnClickListener(onClickListener);
+        /**
+         * 发送一条广播
+         */
+        Button send = (Button) findViewById(R.id.bt_id_send);
+        send.setOnClickListener(onClickListener);
+        /**
+         * 发送一条本地广播
+         */
+        Button sendlocal = (Button) findViewById(R.id.bt_id_sendlocal);
+        sendlocal.setOnClickListener(onClickListener);
+        /**
+         * 注册广播接收器
+         */
+        IntentFilter filterHight = new IntentFilter();
+        filterHight.setPriority(100);
+        filterHight.addAction(BROADCAST_ACTION);
+        registerReceiver(twoBroadcastReciver, filterHight);
+        IntentFilter filterLow = new IntentFilter();
+        filterLow.setPriority(110);
+        filterLow.addAction(BROADCAST_ACTION);
+        registerReceiver(oneBroadcastReciver, filterLow);
+        /**
+         * 注册本地广播接收器
+         */
+        LocalBroadcastManager.getInstance(this).registerReceiver(oneBroadcastReciver,filterLow);
+        LocalBroadcastManager.getInstance(this).registerReceiver(twoBroadcastReciver,filterHight);
     }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.activity_dialog:
+                    Intent dialogIntent = new Intent(MainActivity.this,DialogActivity.class);
+                    startActivity(dialogIntent);
+                    break;
+                case R.id.bt_id_send:
+                    //发送普通广播
+                    /*Intent broadcastIntent = new Intent();
+                    broadcastIntent.setAction(BROADCAST_ACTION);
+                    sendBroadcast(broadcastIntent);*/
+                    //发送有序广播
+                    Intent serviceIntent = new Intent(MainActivity.this,WeatherService.class);
+                    startService(serviceIntent);
+                    break;
+                case R.id.bt_id_sendlocal:
+                    Intent broadcastIntent = new Intent();
+                    broadcastIntent.setAction(BROADCAST_ACTION);
+                    LocalBroadcastManager.getInstance(MainActivity.this)
+                            .sendBroadcast(broadcastIntent);
+            }
+        }
+    };
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -60,6 +137,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         Log.i("Activity","onDestroy");
+        unregisterReceiver(oneBroadcastReciver);
+        unregisterReceiver(twoBroadcastReciver);
         super.onDestroy();
     }
 
